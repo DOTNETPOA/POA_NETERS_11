8212f88ec9035bd0abf58d75fff3a54782b4f03f
Murillo Grubler <msgrubler@gmail.com>
2018-09-23 18:22:04 -0300

    Atualizando informações da apresentação

--- Talk_01/README.md
+++ Talk_01/README.md
@@ -7,4 +7,16 @@ O objetivo dessa Talk é apresentar alguns serviços cognitivos da Microsoft par
 * Serviços Cognitivos
     * Computer Vision
     * Custom Vision
-* <a href='https://github.com/Murillo/Cognitives-Services'>Demonstração</a>
+* <a href='https://analyzingimages.azurewebsites.net/'>Demonstração Computer Vision</a>
+* <a href='https://predictimages.azurewebsites.net/'>Demonstração Custom Vision</a>
+
+## Referêncais
+* <a href='https://medium.com/brasil-ai/entendendo-o-funcionamento-de-uma-rede-neural-artificial-4463fcf44dd0'>Entendendo o funcionamento de uma Rede Neural Artificial</a>
+* <a href='https://medium.com/brasil-ai/classificando-imagens-com-o-custom-vision-da-microsoft-c6ee54aba953'>Classificando imagens com o Custom Vision da Microsoft </a>
+* <a href='https://medium.com/brasil-ai/analisando-imagens-com-computer-vision-api-da-microsoft-520ef28d8eaf'>Analisando imagens com Computer Vision API da Microsoft</a>
+* <a href='http://cs231n.github.io/convolutional-networks/'>Convolutional Neural Networks (CNNs / ConvNets)</a>
+
+## Contato
+* E-mail: msgrubler@gmail.com
+* GitHub: <a href='https://github.com/Murillo'>github.com/murillo</a>
+* Twitter: <a href='https://twitter.com/msgrubler'>@msgrubler</a>
---
+++ Talk_01/src/ComputerVision/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ComputerVision.Models;
+using ComputerVision.ViewModel;
+using ComputerVision.Api;
+using Newtonsoft.Json;
+
+namespace ComputerVision.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string image)
+        {
+            // That variables will load key and url where will be processed from Microsoft
+            var key = "<KEY>";
+            var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+
+            // This code predict informations concern image from API Microsoft
+            var customVision = new ComputerVisionAnalysis(uriBase, key);
+            var result = JsonConvert.DeserializeObject<AnalysisViewModel>(
+                await customVision.MakeAnalysis(image));
+            
+            // Set an image url in object
+            result.UrlImage = image;
+
+            // Return a view and the object that will be processed
+            return View("Index", result);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
